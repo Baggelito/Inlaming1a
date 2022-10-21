0b553bb66ac8d123304ca43890febfb2e893790d
Baggelito <116160737+Baggelito@users.noreply.github.com>
2022-10-21 17:04:50 +0200

    Add files via upload

---
+++ Inläming1,c.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+// Gör subrationen mellan talen. 
+double subtract(double* x,
+				double* y,
+				double* z)
+{
+	 *z = *x - *y; // returnerar talet som c blir
+}
+
+
+int main()
+{
+	double a = 3.4;  // ger a värdet 3.4
+	double b = 6.8; // ger b värdet 6.8
+	double c = 0; // gör c till en float 
+
+
+	
+	subtract(&a, &b, &c);
+	
+	printf("%lg - %lg =%lg\n", a, b, c); // skriver ut vad a - b = c är
+
+
+
+
+	return;
+}
